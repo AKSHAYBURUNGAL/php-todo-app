593933a269f7a14c4a71824cf5cdcd2e2ddd9acf
Akshay Burungale <126006109+AKSHAYBURUNGAL@users.noreply.github.com>
2025-05-04 10:45:48 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
-$host = "localhost";
+// Use the Kubernetes Service name instead of localhost
+$host = "mysql-service";  // Must match your MySQL Service name
 $user = "root";
-$password = "";
+$password = "1122";  // Must match MYSQL_ROOT_PASSWORD
 $dbname = "todo_app";
 
 $conn = new mysqli($host, $user, $password, $dbname);
@@ -10,4 +11,3 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
->
